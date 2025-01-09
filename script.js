384ddadc4dff064bd7b8cd2765d79468e1405392
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-09 13:56:18 -0600

    library 0.0.4
    - wrote function for adding book to display
    - minor styling for individual book divs in display
    - js comments for readability

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const bookTitle = document.getElementById("title");
 const bookPages = document.getElementById("pages");
 const bookRead = document.getElementById("read");
 const submitBtn = document.getElementById("submit");
+const bookViewer = document.getElementById("display");
 
 const myLibrary = [];
 
+//book constructor
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
@@ -13,9 +15,23 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
+//add to library array
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  displayBook(book);
   console.log(book);
+}
+
+//show book in main display
+function displayBook(book) {
+  bookViewer.innerHTML += `
+    <div class="books">
+        <div>${book.author}</div>
+        <div>${book.title}</div>
+        <div>${book.pages}</div>
+        <div>${book.read}</div>
+      </div>
+  `
 }
 
 submitBtn.addEventListener("click", (event) => {
@@ -28,6 +44,7 @@ submitBtn.addEventListener("click", (event) => {
       bookRead.checked
     )
   );
+  //reset form values
   bookAuthor.value = "";
   bookTitle.value = "";
   bookPages.value = "";
